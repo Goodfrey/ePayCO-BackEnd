d94cfa38405d4d540e3483df1d0f7d8bf3aa3add
Luis Campos <campos.luis19@gmail.com>
2022-08-30 18:17:05 -0400

    Update Models
    Actualizacion de modelos Usuarios y Invoices

--- app/Models/Invoices.php
+++ app/Models/Invoices.php
@@ -8,4 +8,58 @@ use Illuminate\Database\Eloquent\Model;
 class Invoices extends Model
 {
     use HasFactory;
+
+    public $fillable =  [ 'id', 'code', 'user_id', 'amount', 'session', 'token', 'status_id', 'created_at'];
+
+    public static function GetValue()
+    {
+        return rand(5, 30)/100 + rand(5, 30);
+    }
+
+    public static function CreateInvo($data)
+    {
+
+        try {
+            $inv                =   new Invoices;
+            $inv->code          =   rand(10000,99999);
+            $inv->user_id       =   $data['user'];
+            $inv->amount        =   $data['amount'];
+            $inv->session       =   $data['session'];
+            $inv->token         =   $data['token'];
+            $inv->status_id     =   1;
+            return ( ($inv->save()) ? true : false);
+            
+        } catch (\Exception $e) {
+           return $e->getMessage();
+        }
+
+    }
+
+    public static function ValidateInfo($iData)
+    {
+        return ( (isset($iData['token'])) && (isset($iData['session'])) ) ? true : false;
+    }
+
+    public static function GetInvo($info)
+    {
+
+        $invo   =   Invoices::where([
+            ['token',       '=', $info['token']],
+            ['session',     '=', $info['session']],
+            ['status_id',   '=', 1]
+        ])->get();
+
+        return (COUNT($invo) == 1) ? $invo : false;
+    }
+
+    public static function UpdateInvo($info)
+    {
+        $invo   =   Invoices::where('code', '=', $info)->get();
+
+        $invo[0]->status_id    =   2;
+        return ($invo[0]->save()) ? true : false;
+    }
+
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+use Illuminate\Database\Eloquent\Model;
+
+use Bavix\Wallet\Traits\HasWallet;
+use Bavix\Wallet\Traits\HasWallets;
+use Bavix\Wallet\Interfaces\Wallet;
+use Bavix\Wallet\Traits\HasWalletFloat;
+use Bavix\Wallet\Interfaces\WalletFloat;
+
+class User extends Model implements Wallet, WalletFloat
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasWallet,  HasWallets, HasWalletFloat;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
+    public $fillable =  [ 'id', 'name', 'email', 'password', 'celular', 'documento'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +46,100 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function createData($res)
+    {
+        try {
+            $profile    =   date('YmdHms');
+            $user   =   new User;
+            $user->name         =   $res['nombre'];
+            $user->email        =   $res['email'];
+            $user->celular      =   $res['celular'];
+            $user->profile      =   $profile;
+            $user->documento    =   $res['documento'];
+            $user->password     =   bcrypt('User.01');
+            
+            if( $user->save() )
+            {
+                $user->createWallet([
+                    'name'          =>  $user->profile,
+                    'slug'          =>  $user->profile,
+                    'meta'          =>  'USD',
+                    'description'   =>  'Wallet Client: '.$user->name.'',
+                ]);
+
+                return true;
+
+            }else{
+                false;
+            }
+            
+        } catch (\Exception $e) {
+           return false;
+        }
+    }
+
+    public static function validateAll($res)
+    {
+        $nom    = ( isset($res['nombre']) )     ? true : false;
+        $ema    = ( isset($res['email']) )      ? true : false;
+        $cel    = ( isset($res['celular']) )    ? ( (is_numeric($res['celular']) ? true : false) ) : false;
+        $doc    = ( isset($res['documento']) )  ? true : false;
+
+        if ( ($nom == true) && ($ema == true) && ($cel == true) && ($doc == true) )
+        {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public static function validateRecharge($res)
+    {
+        $doc    = ( isset($res['documento']) ) ? true : false;
+        $cel    = ( isset($res['celular']) )  ? ( (is_numeric($res['celular']) ? true : false) ) : false;
+
+        if ( ($doc == true) && ($cel == true) )
+        {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public static function validateAmount($res)
+    {
+        return ( isset($res['valor']) )  ? ( (is_numeric($res['valor']) ? (( $res['valor'] > 0 ) ? true : false) : false) ) : false;
+    }    
+
+
+
+    public static function validatePhone($res)
+    {
+        if(isset($res['celular']) == true)
+        {
+            return (is_numeric($res['celular'])) ? true : false;
+        }else{
+            return false;
+        }
+    }
+
+    public static function ValidateUser($info)
+    {
+
+        $info   =   User::where([
+            ['documento',   '=', $info['documento']],
+            ['celular',     '=', $info['celular']]
+        ])->get();
+
+        return (COUNT($info) == 0) ? false : $info;
+    }
+
+    public static function UserByID($res)
+    {
+        $info   =   User::where('id', '=', $res)->get();
+        return (COUNT($info) == 0) ? false : $info;
+    }
 }
